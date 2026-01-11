18b8282d7ba5259527b815ab70fe54b8cca32567
abhinav11 <abhinavpandey2405@gmail.com>
2026-01-11 13:05:19 +0530

    final

--- server.js
+++ server.js
@@ -1,37 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "../routes/auth.js";
+import userRoutes from "../routes/user.js";
+import adminRoutes from "../routes/admin.js";
+
+dotenv.config();
 
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/admin", adminRoutes);
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/user', require('./routes/user'));
-app.use('/api/admin', require('./routes/admin'));
-
-app.get('/', (req, res) => {
-  res.json({ msg: 'Competition Backend Running!' });
+app.get("/api", (req, res) => {
+  res.json({ msg: "Competition Backend Running on Vercel!" });
 });
 
 
-const startServer = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB Connected');
+let isConnected = false;
+async function connectDB() {
+  if (isConnected) return;
+  await mongoose.connect(process.env.MONGO_URI);
+  isConnected = true;
+  console.log("MongoDB Connected");
+}
 
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () =>
-      console.log(`Server running at http://localhost:${PORT}`)
-    );
-  } catch (err) {
-    console.error('MongoDB connection failed:', err.message);
-    process.exit(1); 
-  }
-};
 
-startServer();
+export default async function handler(req, res) {
+  await connectDB();
+  return app(req, res);
+}
