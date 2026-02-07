a8fc6805b583715d7fa4b7bc4e57ca1dfb38d514
Shourya Veer Singh <shourya31veer@gmail.com>
2026-02-07 23:47:00 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ router.get('/leaderboard', async (req, res) => {
   const leaderboard = await User.find()
       .select('name totalPoints eventScores')
       .sort({ totalPoints: -1 })
-      .limit(20);
+      .limit(100);
       
 
   res.json({ success: true, leaderboard });
